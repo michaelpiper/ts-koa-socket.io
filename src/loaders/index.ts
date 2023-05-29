da22723c967813925b09646eb63bb62ebd0eb8be
Michael Piper <pipermichael@aol.com>
2023-05-29 11:36:12 +0100

    added fixes

--- src/loaders/index.ts
+++ src/loaders/index.ts
@@ -2,7 +2,8 @@ import zeroant from './zeroant.js'
 import Config from '../common/config/index.js'
 import { Plugin } from '../common/plugins/index.js'
 import Registry from '../common/registry.js'
-export default async (customConfig: Record<string, string | undefined> = {}) => {
+import { type CustomConfig } from '../common/config/config.js'
+export default async (customConfig: CustomConfig = {}) => {
   const plugins = new Plugin(zeroant)
   const config = new Config(Object.assign({}, process.env, customConfig))
   for (const addon of Registry.configs) {
